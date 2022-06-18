4bf33b073ec39dc1f27e40c6fd0d68460e5762fc
Shailesh Aanand <anaandshailu@gmail.com>
2022-06-18 22:30:52 +0530

    add search to patients

--- src/pages/Patients.jsx
+++ src/pages/Patients.jsx
@@ -1,30 +1,80 @@
 import { Table } from "@mantine/core";
 import { Box } from "@mantine/core";
+import { Center } from "@mantine/core";
+import { Pagination } from "@mantine/core";
 import { Group } from "@mantine/core";
 import { ActionIcon } from "@mantine/core";
+import { Space } from "@mantine/core";
+import { TextInput } from "@mantine/core";
+import { Select } from "@mantine/core";
 import { Title } from "@mantine/core";
+import { useDebouncedValue } from "@mantine/hooks";
 import { Link } from "raviger";
+import { useState } from "react";
 import { useQuery } from "react-query";
 import { Trash } from "tabler-icons-react";
 import { Pencil } from "tabler-icons-react";
 import { listPatients } from "../api/api";
 import NewPatient from "../components/buttons/NewPatient";
+import { omit } from "../helpers/utils";
 
 const Patients = () => {
+  const [limit, setLimit] = useState("10");
+  const [page, setPage] = useState(1);
+  const [limitOptions, setLimitOptions] = useState(["10", "25", "50", "100"]);
+  const [search, setSearch] = useState("");
+  const [name] = useDebouncedValue(search, 500);
   const { data } = useQuery(
-    "listPatients",
+    ["listPatients", { limit, page, name }],
     async () => {
-      const response = await listPatients();
+      const response = await listPatients(
+        omit({
+          limit: parseInt(limit),
+          page,
+          name,
+        })
+      );
       return response[1];
     },
-    {}
+    {
+      keepPreviousData: true,
+    }
   );
   return (
     <Box m={30}>
       <Title order={2} mb={20}>
         Patients
       </Title>
-      <NewPatient />
+      <NewPatient mb={20} />
+      <Group align={"end"}>
+        <Box sx={(theme) => ({ width: "100px" })}>
+          <Select
+            data={limitOptions}
+            value={limit}
+            label="Rows"
+            onChange={setLimit}
+            creatable
+            searchable
+            getCreateLabel={(query) => `+ Create ${query}`}
+            onCreate={(value) => {
+              setLimitOptions(
+                [...limitOptions, value].sort(
+                  (a, b) => parseInt(a) - parseInt(b)
+                )
+              );
+              setLimit(value);
+            }}
+          />
+        </Box>
+        <TextInput
+          placeholder="Search"
+          value={search}
+          onChange={(e) => {
+            setSearch(e.currentTarget.value);
+          }}
+        />
+      </Group>
+      <Space h={"md"} />
       <Box mt={20}>
         <Table>
           <thead>
@@ -38,8 +88,8 @@ const Patients = () => {
             </tr>
           </thead>
           <tbody>
-            {data &&
-              data.map((patient) => (
+            {data?.data &&
+              data.data.map((patient) => (
                 <tr key={patient.id}>
                   <td>{patient.name}</td>
                   <td>{patient.phone}</td>
@@ -64,6 +114,10 @@ const Patients = () => {
               ))}
           </tbody>
         </Table>
+        <Space h="md" />
+        <Center>
+          <Pagination page={page} total={data?.totalPages} onChange={setPage} />
+        </Center>
       </Box>
     </Box>
   );
