58626d49c205fd025270dd170b6e9d2a0fd8f508
Shailesh Aanand <anaandshailu@gmail.com>
2022-06-23 23:43:04 +0530

    makr firerequest async

--- src/api/fire-request.jsx
+++ src/api/fire-request.jsx
@@ -44,64 +44,57 @@ export const FireRequest = async (
   }
 
   // calling fetch api
-  const response = fetch(baseUrl + path, options)
-    .then((response) => {
-      const data = response.json();
-      return Promise.all([response, data]);
-    })
-    .then((res) => {
-      if (res[0].status === 401) {
-        // toast.error("Unauthorized access denied");
-        localStorage.removeItem("token");
-      } else if (res[0].status === 405) showNotification("Method not allowed");
-      else if (res[0].status >= 400) {
-        for (var k in res[1]) {
-          if (typeof res[1][k] === "string") {
-            // toast.error(
-            //   res[1][k].replace(/\w\S*/g, (w) =>
-            //     w.replace(/^\w/, (c) => c.toUpperCase())
-            //   )
-            // );
-            showNotification({
-              message: res[1][k].replace(/\w\S*/g, (w) =>
-                w.replace(/^\w/, (c) => c.toUpperCase())
-              ),
-              title: "Error",
-              icon: <X />,
-              color: "red",
-            });
-            console.log(res[1][k]);
-          } else {
-            // toast.error(
-            //   res[1][k][0].replace(/\w\S*/g, (w) =>
-            //     w.replace(/^\w/, (c) => c.toUpperCase())
-            //   )
-            // );
-
-            showNotification({
-              message: res[1][k][0].replace(/\w\S*/g, (w) =>
-                w.replace(/^\w/, (c) => c.toUpperCase())
-              ),
-              title: "Error",
-              icon: <X />,
-              color: "red",
-            });
-            console.log(res[1][k][0]);
-          }
-        }
-      }
-
-      return res;
-    })
-    .catch((err) => {
-      // toast.error("Something went wrong");
+  try {
+    const response = await fetch(baseUrl + path, options);
+    let data = {};
+    try {
+      data = await response.json();
+    } catch (error) {
+      data = {};
+    }
+    if (response.status === 401) {
       showNotification({
-        message: "Something went wrong",
+        message: "Unauthorized access denied",
         title: "Error",
         icon: <X />,
         color: "red",
       });
-      console.log(err);
+      localStorage.removeItem("token");
+    } else if (response.status === 405) {
+      showNotification("Method not allowed");
+    } else if (response.status >= 400) {
+      for (var k in data) {
+        if (typeof data[k] === "string") {
+          showNotification({
+            message: data[k].replace(/\w\S*/g, (w) =>
+              w.replace(/^\w/, (c) => c.toUpperCase())
+            ),
+            title: "Error",
+            icon: <X />,
+            color: "red",
+          });
+          console.log(data[k]);
+        } else {
+          showNotification({
+            message: data[k][0].replace(/\w\S*/g, (w) =>
+              w.replace(/^\w/, (c) => c.toUpperCase())
+            ),
+            title: "Error",
+            icon: <X />,
+            color: "red",
+          });
+          console.log(data[k][0]);
+        }
+      }
+    }
+    return [response, data];
+  } catch (err) {
+    showNotification({
+      message: "Something went wrong",
+      title: "Error",
+      icon: <X />,
+      color: "red",
     });
-  return response;
+    console.log(err);
+  }
 };
