0a6245b8655e729900d3790d935fa235e33293f8
Shailesh Aanand <anaandshailu@gmail.com>
2022-10-23 13:09:00 +0530

    Change to GCR backend

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,2 +1,2 @@
-export const API_URL = "https://labreporter-staging.herokuapp.com/api/v1";
+export const API_URL = "https://lab-reporter-staging-pqkvl4xl7q-em.a.run.app/api/v1";
 export const DEFAULT_PAGE_SIZE = 10;
