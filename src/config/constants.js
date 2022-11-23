eded16738caf220107a77fa4052fe337231e11d2
Shailesh Aanand <anaandshailu@gmail.com>
2022-11-23 19:58:00 +0530

    add docker support

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,2 +1,4 @@
-export const API_URL = "https://labreporter-staging.herokuapp.com/api/v1";
+// export const API_URL = "https://labreporter-staging.herokuapp.com/api/v1";
+export const API_URL = import.meta.env.VITE_BACKEND_URL + "/api/v1";
+console.log("API_URL", API_URL);
 export const DEFAULT_PAGE_SIZE = 10;
