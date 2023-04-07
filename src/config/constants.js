badffc49da306102ac453d02a8490bc24a856822
Shailesh Aanand <anaandshailu@gmail.com>
2023-04-07 16:43:43 +0530

    update to new backend server

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,3 +1,3 @@
 export const API_URL =
-  "https://lab-reporter-prod-pqkvl4xl7q-an.a.run.app/api/v1";
+  "https://lab-reporter-ane1-prod-pqkvl4xl7q-an.a.run.app/api/v1";
 export const DEFAULT_PAGE_SIZE = 10;
